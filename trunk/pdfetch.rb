fbec0ec5ccbcfbdcc30de99c121827306ac681e3
edoardo.marcora <edoardo.marcora@163144cb-901f-0410-9ff2-cf3062207263>
2006-10-13 03:12:27 +0000

    Skip fetching if file already exists.

--- trunk/pdfetch.rb
+++ trunk/pdfetch.rb
@@ -1,10 +1,5 @@
 require 'camping'
-
-require 'open-uri'
-require 'cgi'
-
 require 'bio'
-require 'hpricot'
 require 'mechanize'
 
 Camping.goes :Pdfetch
@@ -17,7 +12,7 @@ module Pdfetch::Controllers
       @body = %{
 
 /* main.css */
-body { margin: 0; padding: 10; font-size: small; font-family: arial, sans; }
+body { margin: 0; padding: 10; font-size: medium; font-family: arial, sans; }
 a { text-decoration: none;}
 h1 { font-size: 110%; }
 p { font-size: 90%; }
@@ -26,32 +21,41 @@ p { font-size: 90%; }
     end
   end
 
-  class Index < R '/(\d+)'
+  class Static < R '/(\d+)\.pdf$'         
     def get(id)
+      @pmid = id
+      @headers['Content-Type'] = "application/pdf"
+      @headers['X-Sendfile'] = "#{Dir.getwd}/#{id}.pdf"
+    end
+  end 
+  
+  class Index < R '/(\d+)$'
+    def get(id)
+      @pmid = id
       begin
-        # fetch the article from pubmed using pmid
-        @article = Bio::MEDLINE.new(Bio::PubMed.query(id))
-        m = WWW::Mechanize.new
-        p = m.get("http://eutils.ncbi.nlm.nih.gov/entrez/eutils/elink.fcgi?dbfrom=pubmed&id=#{id}&retmode=ref&cmd=prlinks")
-      
-        if @article.journal.strip =~ /^nature/i
-          p = m.click p.links.with.text(/full/i).and.href(/full/i)
-          p = m.click p.links.with.href(/.pdf$/i)
-          p.save_as("#{id}.pdf")
-        elsif frame = p.frames.with.name(/reprint/i) and not frame.empty?
-          p = m.click frame
-          p = m.click p.links.with.href(/.pdf$/i)
-          p.save_as("#{id}.pdf")
-        elsif link = p.links.with.text(/pdf/i).and.href(/.pdf$/i) and not link.empty?
-          p = m.click link
-          p.save_as("#{id}.pdf")
-        else
-          p = m.click p.links.with.text(/pdf/i).and.href(/reprint/i)
-          p = m.click p.frames.with.name(/reprint/i)
-          p = m.click p.links.with.href(/.pdf$/i)
-          p.save_as("#{id}.pdf")
+        unless File.exist?("#{id}.pdf")
+          # fetch the article from pubmed using pmid
+          @article = Bio::MEDLINE.new(Bio::PubMed.query(id))
+          m = WWW::Mechanize.new
+          p = m.get("http://eutils.ncbi.nlm.nih.gov/entrez/eutils/elink.fcgi?dbfrom=pubmed&id=#{id}&retmode=ref&cmd=prlinks")
+          if @article.journal.strip =~ /^nature/i
+            p = m.click p.links.with.text(/full/i).and.href(/full/i)
+            p = m.click p.links.with.href(/.pdf$/i)
+            p.save_as("#{id}.pdf")
+          elsif frame = p.frames.with.name(/reprint/i) and not frame.empty?
+            p = m.click frame
+            p = m.click p.links.with.href(/.pdf$/i)
+            p.save_as("#{id}.pdf")
+          elsif link = p.links.with.text(/pdf/i).and.href(/.pdf$/i) and not link.empty?
+            p = m.click link
+            p.save_as("#{id}.pdf")
+          else
+            p = m.click p.links.with.text(/pdf/i).and.href(/reprint/i)
+            p = m.click p.frames.with.name(/reprint/i)
+            p = m.click p.links.with.href(/.pdf$/i)
+            p.save_as("#{id}.pdf")
+          end
         end
-
         render :index
       rescue
         render :error
@@ -66,12 +70,12 @@ module Pdfetch::Views
     xhtml_strict do
       head do
         link :rel => 'stylesheet', :type => 'text/css', :href => '/main.css', :media => 'screen'
-        script "function goback(){window.history.back()}function waitnback(){window.setTimeout(goback(),3000);}", :type => 'text/javascript'
+        script "function goback(){window.history.back()}function waitngoback(){window.setTimeout(goback(),3000);}", :type => 'text/javascript'
       end
-#      body :onload => 'waitnback()' do
+#      body :onload => 'waitngoback()' do
       body do
         self << yield
-        p { "Click #{a 'here', :href => '#', :onclick => 'goback()'} to go back." }
+        p { "Click #{a 'here', :href => '#', :onclick => 'goback()'} to go back, or click #{a "here", :href => "#{@pmid}.pdf"} to view the reprint." }
       end
     end
   end
